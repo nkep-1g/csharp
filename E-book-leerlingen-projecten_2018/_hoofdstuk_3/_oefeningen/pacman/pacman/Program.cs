392d728a3910337edf223dc0142512fd1d3edfff
Joris  Custers | SFE Personeel <joris.custers@sfevergem.be>
2024-12-10 13:43:13 +0100

    adding oefening pacman as console project

---
+++ E-book-leerlingen-projecten_2018/_hoofdstuk_3/_oefeningen/pacman/pacman/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Pacman // Note: actual namespace depends on the project name.
+{
+
+    class Pacman
+    {
+        private String _naam;
+        private int _aantalLevens;
+        private bool _isOnKwetsbaar;
+        public Pacman(String naam, int aantalLevens)
+        {
+            _naam = naam;
+            _aantalLevens = aantalLevens;
+            _isOnKwetsbaar = false;
+        }
+
+        public String Naam
+        { 
+            get { return _naam; } 
+            set { _naam = value; }
+        }
+
+        public int AantalLevens
+        {
+            get { return _aantalLevens; }           
+        }
+
+        public bool IsOnkwetsbaar
+        {
+            get { return _isOnKwetsbaar; }
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pacman x = new Pacman("Joris", 7);
+            int y = x.AantalLevens;
+            Console.WriteLine("Hello World!!!" + y);
+        }
+    }
+}
