1708b183867f74f378cee5170ef204859b8d27eb
Joris  Custers | SFE Personeel <joris.custers@sfevergem.be>
2024-12-10 13:57:33 +0100

    adding smartphone code to repos

---
+++ E-book-leerlingen-projecten_2018/_hoofdstuk_3/_oefeningen/smartphone/smartphone/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smartphone
+{
+    internal class Program
+    {
+        public class Smartphone
+        { 
+            private String _telnummer;
+            private String _pincode;
+            public Smartphone(String telnummer, String pincode) 
+            {
+                _telnummer = telnummer;
+                _pincode = pincode;            
+            }
+
+            public String Telnummer
+            {
+                get { return _telnummer; }
+                set { _telnummer = value; }
+            }
+
+            public String Pincode
+            { 
+                get { return _pincode; } 
+                set { _pincode = value; }
+            }         
+        }
+
+        static void Main(string[] args)
+        {
+            Smartphone smartPhone = new Smartphone("nummer", "1234");
+            smartPhone.Telnummer = "0490110289";
+            smartPhone.Pincode = "3698";
+            Console.WriteLine(smartPhone.Pincode);
+            Console.WriteLine(smartPhone.Telnummer);
+            Console.ReadLine();
+        }
+    }
+}
